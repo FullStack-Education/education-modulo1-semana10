518ca9dcdadb97b0e32e21606bef2c2b6afb3eeb
Gabriel Augustin <gabriel.augustin@edu.sc.senai.br>
2024-03-19 22:28:15 -0300

    feat: Adicionado filtro em GET /usuarios e tratado Status HTTP nos endpoints

--- src/main/java/br/com/fullstack/education/m1s10/controller/UsuarioController.java
+++ src/main/java/br/com/fullstack/education/m1s10/controller/UsuarioController.java
@@ -1,24 +1,28 @@
 package br.com.fullstack.education.m1s10.controller;
 
+import br.com.fullstack.education.m1s10.dto.UsuarioFiltro;
 import br.com.fullstack.education.m1s10.entity.UsuarioEntity;
 import br.com.fullstack.education.m1s10.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("usuarios")
 public class UsuarioController {
 
     private final UsuarioService service;
 
-    public UsuarioController(UsuarioService service) {
-        this.service = service;
-    }
-
     @GetMapping
-    public List<UsuarioEntity> get() {
-        return service.buscarTodos();
+    public ResponseEntity<List<UsuarioEntity>> get(UsuarioFiltro filtro) {
+        List<UsuarioEntity> usuarios = service.buscarTodos(filtro);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarios);
     }
 
     @GetMapping("{id}")
@@ -26,9 +30,15 @@ public class UsuarioController {
         return service.buscarPorId(id);
     }
 
+    @GetMapping("login/{login}")
+    public UsuarioEntity getLogin(@PathVariable String login) throws Exception {
+        return service.buscarPorLogin(login);
+    }
+
     @PostMapping
-    public UsuarioEntity post(@RequestBody UsuarioEntity usuario) throws Exception {
-        return service.criar(usuario);
+    public ResponseEntity<UsuarioEntity> post(@RequestBody UsuarioEntity usuario) throws Exception {
+        UsuarioEntity usuarioCriado = service.criar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
     }
 
     @PutMapping("{id}")
